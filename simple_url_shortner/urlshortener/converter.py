0a8d912bab1729962f63262ab1caf7e7408dee5f
Ricardo L. Dani <ricardodani@gmail.com>
2013-11-06 20:37:50 -0200

    Use string module to define CHARS constant.

--- simple_url_shortner/urlshortener/converter.py
+++ simple_url_shortner/urlshortener/converter.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
 
-CHARS = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHUJKLMNOPQRSTUVWXYZ'
+import string
+CHARS = string.digits + string.letters
 
 def num_to_base62(n):
     '''
-    Convert a decimal number in a base62 (decimals + lower + uppercase letters)
+    Convert a decimal number in a base62 (digits + lower & uppercase letters)
     '''
     if (n >= 62):
         return num_to_base62(int(n / 62.)) + CHARS[n % 62]
