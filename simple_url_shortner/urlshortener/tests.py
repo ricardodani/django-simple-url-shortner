7eb9a2a33cfed7b880004bfbb0d0fd8c9e203367
Ricardo L. Dani <ricardodani@gmail.com>
2013-11-06 20:38:46 -0200

    Added some tests.

--- simple_url_shortner/urlshortener/tests.py
+++ simple_url_shortner/urlshortener/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from converter import num_to_base62
+from models import Url
 
-# Create your tests here.
+class ConverterTestCase(TestCase):
+
+    def setUp(self):
+        Url.objects.create(id=70, url="http://google.com")
+
+    def test_find_url_by_shortcode(self):
+        url = Url.objects.get(short_code=num_to_base62(70))
+        self.assertEqual(url.id, 70)
+
+    def test_url_have_correct_shortcode(self):
+        url = Url.objects.get(id=70)
+        self.assertEqual(url.short_code, num_to_base62(70))
