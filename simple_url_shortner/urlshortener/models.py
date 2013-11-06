5f40984cbe7e655f91e40032170f4884f60b3695
Ricardo L. Dani <ricardodani@gmail.com>
2013-11-06 20:38:36 -0200

    Added auto updated short_code on save.

--- simple_url_shortner/urlshortener/models.py
+++ simple_url_shortner/urlshortener/models.py
@@ -3,6 +3,7 @@
 import sys
 from django.db import models
 from django.contrib.auth.models import User
+from converter import num_to_base62
 
 
 class Url(models.Model):
@@ -10,6 +11,16 @@ class Url(models.Model):
     Model representing a shortened URL.
     '''
     url = models.URLField()
-    short_code = models.CharField(max_length, db_index=True)
+    short_code = models.CharField(max_length=20, db_index=True)
     user = models.ForeignKey(User, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def __unicode__(self):
+        return u'%s => %s' % (self.short_code, self.url)
+
+    def save(self, *args, **kwargs):
+        super(Url, self).save(*args, **kwargs)
+        Url.objects.filter(id=self.id).update(short_code=num_to_base62(self.id))
+
+    class Meta:
+        ordering = ['-created_at']
