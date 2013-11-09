7fb73b46bfe7df3ebdf62496c09f72c6a58f2ac3
Ricardo L. Dani <ricardodani@gmail.com>
2013-11-09 21:06:58 -0200

    Added SITE_URL.

--- README.md
+++ README.md
@@ -2,3 +2,12 @@ django-simple-url-shortner
 ==========================
 
 A simple URL Shortner project written in python/django.
+
+Settings
+--------
+
+Change ``SITE_URL`` settings to your correct domain.
+
+Example:
+
+    SITE_URL = 'http://localhost:8000'
--- simple_url_shortner/simple_url_shortner/settings.py
+++ simple_url_shortner/simple_url_shortner/settings.py
@@ -8,6 +8,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.6/ref/settings/
 """
 
+SITE_URL = 'http://localhost:8000'
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
