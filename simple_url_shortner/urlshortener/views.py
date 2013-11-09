6b39174f0117052c06bf6bd291b3c58cf7f885e2
Ricardo L. Dani <ricardodani@gmail.com>
2013-11-09 11:38:11 -0200

    Update views.py

--- simple_url_shortner/urlshortener/views.py
+++ simple_url_shortner/urlshortener/views.py
@@ -5,19 +5,22 @@ from .models import Url
 
 def index(request):
     """
-    Main View, show form and list URL's of the authenticated user.
+    Main View, show form and list Url`s of the authenticated user.
     """
     if request.method == "POST":
         form = UrlCreateForm(request.POST)
         if form.is_valid():
             form.save()
-            # message.success with URL ?
-            # redirect to GET home
+            # get short_url, set short_url in context
+            # context['short_url'] = num_to_b62(request.POST.get('url')) # <- DRY Alert.
     else:
         form = UrlCreateForm()
 
+    usr = request.user
     context = {
-        'url_list': Url.objects.all(),
+        # Returns the users ``Url.objects`` QuerySet or None if Anonymous.
+        'url_list': Url.objects.filter(user=usr) if usr.is_authenticated() else None,
+        # Form to insert url to shortener
         'create_form': form,
     }
     return render(request, 'index.html', context)
